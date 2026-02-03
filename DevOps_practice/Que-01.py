e683671012a86be1bd147593b75ea7fc5d7ce3b6
SaiKrishna329-Dev <gsaikrishna1496@gmail.com>
2026-02-03 11:22:42 +0530

    interview questions

--- DevOps_practice/Que-01.py
+++ DevOps_practice/Que-01.py
@@ -19,6 +19,62 @@ for i in directories:
 
 
 
+#Delete or archive files older than X days.
+ 
+import os
+import time
+import glob
+
+
+folder = ['/var/log/*']
+days = 7
+now = time.time()
+
+cutoff = now - (days*86400)
+
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
+
+    if os.path.isfile(folder):
+        if os.stat(path).st_mtime < cutoff:
+            print(f"delete the {path}")
+            os.remove(path)    
+        
+# To Archive with glob - first compress then move to archive
+
+import os
+import time
+import glob
+import shutil
+import gzip
+
+SOURCE = "/var/log"
+ARCHIVE = "/var/backup/archive"
+days = 30
+cutoff = time.time() - (days * 86400)
+
+os.makedirs(ARCHIVE, exist_ok=True)
+
+for path in glob.glob(f"{SOURCE}/*.log"):
+    if os.path.isfile(path) and os.stat(path).st_mtime < cutoff:
+
+        filename = os.path.basename(path)
+        compres_file = path + ".gz"
+
+        with open(path, 'rb') as f_in, gzip.open(compres_file, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+        shutil.move(compres_file, os.path.join(ARCHIVE, filename + ".gz"))
+        os.remove(path)
+
+        print(f"Archived: {filename}")    
+
+
+
+
+
+
+
 # * glob module:
 # The glob module in Python is used to search for files and directories whose names match a specified pattern (with wildcards like *, ?, [], etc.).
 
@@ -45,3 +101,9 @@ for i in directories:
 #   '/var/log/private',
 #   ...
 # ]
+
+#In {size_mb:.2f}:
+
+# f → format the number as a floating-point (decimal) value
+
+# .2 → keep 2 digits after the decimal point
